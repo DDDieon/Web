559958f45d1cea6a73818131b49f436d00a71dd2
Jiwon <ajiop0123@gmail.com>
2020-07-16 17:48:25 +0900

    200716 포트폴리오사이트 갤러리 draggable 적용

---
+++ portfolio/jiwon/js/display.js
@@ -0,0 +1,6 @@
+// footer close
+var footerClose = document.getElementById('footerClose');
+var footer = document.getElementById('footer');
+footerClose.onclick = function() {
+    footer.style.display = 'none';
+}
