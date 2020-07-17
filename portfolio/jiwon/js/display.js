50056fb96a0419cb5026749010884282efa6de60
Jiwon <ajiop0123@gmail.com>
2020-07-17 17:50:17 +0900

    200717 웹페이지 스크롤이벤트

--- portfolio/jiwon/js/display.js
+++ portfolio/jiwon/js/display.js
@@ -1,3 +1,25 @@
+// scroll event
+$(function() {
+    var btn = $('.nav_list li button');
+    btn.click(function() {
+        event.preventDefault(); // 기본이벤트 x
+        event.stopPropagation(); // 상위전달x
+        var i = $(this).parent().index(); // li의 i번째
+        if (i == 4) {
+            $(this).off;
+            console.log('click');
+        } else {
+            var sectionTT = $('section').eq(i).offset().top //div들의 y좌표
+            $('html, body').stop().animate({
+                scrollTop: sectionTT //html, body의 y좌표는 i번째 div의 좌표가 된다
+            })
+        }
+
+    })
+})
+
+
+
 // footer close
 var footerClose = document.getElementById('footerClose');
 var footer = document.getElementById('footer');
