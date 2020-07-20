03163740bc818cd0d7557fd37e956f824dee1468
Jiwon <ajiop0123@gmail.com>
2020-07-20 17:49:50 +0900

    200720 포트폴리오사이트 ie10대응 flex->float

---
+++ Smart_Publishing/portfolio/jiwon/js/gallery.js
@@ -0,0 +1,16 @@
+// ------------ gallery open, close-------------
+var galleryIsBody = document.getElementById('gallery-body');
+
+function openGallery() {
+    galleryIsBody.classList.add('galleryIsBody');
+}
+
+function closeGallery() {
+    galleryIsBody.classList.remove('galleryIsBody');
+}
+
+
+//------------ gallery draggable from JQuery UI------------
+$(function() {
+    $('#dragdiv').draggable({ "axis": "x" });
+})
