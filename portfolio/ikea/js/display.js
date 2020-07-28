747f435f1f3dfe3a22fdeea5a9878050bb5ae1e6
Jiwon <ajiop0123@gmail.com>
2020-07-28 17:49:50 +0900

    jquery leran + ikea showroom plus

--- portfolio/ikea/js/display.js
+++ portfolio/ikea/js/display.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     //alert('123');
     $(".slick-items").slick({
         autoplay: true,
-        dots: true,
-        speed: 300 /* 이미지가 슬라이딩시 걸리는 시간 */,
+        dots: false,
+        speed: 600 /* 이미지가 슬라이딩시 걸리는 시간 */,
         infinite: true,
-        autoplaySpeed: 3000 /* 이미지가 다른 이미지로 넘어 갈때의 텀 */,
+        autoplaySpeed: 2000 /* 이미지가 다른 이미지로 넘어 갈때의 텀 */,
         arrows: false,
         slidesToShow: 1,
         slidesToScroll: 1,
