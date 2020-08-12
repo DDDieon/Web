8e92ee14c8f1d05a00f5b4388df99543fb622921
Jiwon <ajiop0123@gmail.com>
2020-08-12 14:42:13 +0900

    200812 jQueryLeaned + react Project edited

--- portfolio/react_matching/src/App.js
+++ portfolio/react_matching/src/App.js
@@ -10,8 +10,7 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            page: "welcome",
-            // welcome: { title: "전체타이틀", desc: "desc is me" },
+            page: "read1",
             data: [
                 {
                     id: 1,
@@ -26,7 +25,7 @@ class App extends Component {
                     title: "포트폴리오2",
                 },
             ],
-            img: [
+            img1: [
                 { id: 101, src: "asset/port2.png" },
                 { id: 102, src: "asset/port3.png" },
                 { id: 103, src: "asset/port4.png" },
@@ -35,31 +34,31 @@ class App extends Component {
                 { id: 105, src: "asset/port7.png" },
                 { id: 105, src: "asset/port8.png" },
             ],
+            img2: [
+                { id: 101, src: "asset/port1.png" },
+                { id: 102, src: "asset/port9.png" },
+            ],
         }
     }
 
     render() {
-        var _title = null
-        if (this.state.mode === "welcome") {
-            _title = this.state.page
-        } else if (this.state.mode === "read") {
-            _title = this.state.img
+        var _title
+        var onChangePage
+        if (this.state.page === "read1") {
+            _title = this.state.img1
+        } else if (this.state.page === "read2") {
+            _title = this.state.img2
         }
-        // var _title = null
-        // var _desc = null
-        // if (this.state.mode === "welcome") {
-        //     _title = this.state.welcome.title
-        //     _desc = this.state.welcome.desc
-        // } else if (this.state.mode === "read") {
-        //     _title = this.state.welcome.title
-        //     _desc = this.state.welcome.desc
-        // }
+
+        onChangePage = () => {
+            this.setState({ page: "read2" })
+        }
 
         return (
             <div className="App">
                 <Header></Header>
                 <Nav data={this.state.data} page={this.state.page}></Nav>
-                <Content img={this.state.img}></Content>
+                <Content img={_title} onChangePage={this.state.onChangePage()}></Content>
             </div>
         )
     }
--- portfolio/react_matching/src/components/Nav.js
+++ portfolio/react_matching/src/components/Nav.js
@@ -4,27 +4,36 @@ class Nav extends Component {
     render() {
         var lists = []
         var data = this.props.data
+        var onChangePage = this.props.onChangePage
         var i = 0
-        while (i < data.length) {
-            lists.push(
-                <li key={"navList" + i}>
-                    <a
-                        href={"/" + data[i].id}
-                        onClick={(e) => {
-                            e.preventDefault()
-                            this.setState({ page: "read" })
-                        }}
-                    >
-                        {data[i].title}
-                    </a>
-                </li>
-            )
-            i++
-        }
+
+        // while (i < data.length) {
+        //     lists.push(
+        //         <li key={"navList" + i}>
+        //             <a
+        //                 href={"/" + data[i].id}
+        //                 onClick={(e) => {
+        //                     e.preventDefault()
+        //                 }}
+        //             >
+        //                 {data[i].title}
+        //             </a>
+        //         </li>
+        //     )
+        //     i++
+        // }
 
         return (
             <div className="nav_bar">
-                <ul className="lists">{lists}</ul>
+                <ul className="lists">
+                    <li>
+                        <a href="/" onClick={onChangePage}>
+                            요약보기
+                        </a>
+                    </li>
+                    <li>포트폴리오</li>
+                    <li>포트폴리오2</li>
+                </ul>
             </div>
         )
     }
