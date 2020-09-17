69fd546e684cdbf037cb2b47e9886a02d312a0f1
Jiwon <59174247+dddieon@users.noreply.github.com>
2020-09-17 15:41:33 +0900

    Setting Modal in photozone

--- portfolio/jiwon/photozone/js/photozone-display.js
+++ portfolio/jiwon/photozone/js/photozone-display.js
@@ -4,7 +4,6 @@ window.onload = function () {
     var urlArray = htmlSplit.length
     var urlHtml = htmlSplit[urlArray - 1]
     var htmlName = urlHtml.split(".")[0]
-    //url에서 htmlName(design.html->design)를 가져와서 css를 변경합니다
     //nav style
     console.log(htmlName)
     document.querySelector("." + htmlName + "DeskMenu").style.color = "#ff3939"
@@ -15,11 +14,10 @@ window.onload = function () {
     document.querySelector(".main-headline--back").style.backgroundSize = "100% auto"
 }
 
-// jQuery - 햄버거/패널창을 디자인합니다
+// jQuery - 햄버거/패널창을 디자인
 $(function () {
     $(".hamburger").on("click", function () {
-        $(".mobile-drawer").css("display", "block").stop().animate(
-            {
+        $(".mobile-drawer").css("display", "block").stop().animate({
                 right: 0,
             },
             500
@@ -28,12 +26,29 @@ $(function () {
     })
 
     $(".closeIcon").on("click", function () {
-        $(".mobile-drawer").css("display", "none").stop().animate(
-            {
+        $(".mobile-drawer").css("display", "none").stop().animate({
                 right: "-100%",
             },
             500
         )
         $("html, body").css("overflow", "auto")
+    })
+
+    // 모달창
+    $(".items").on("click", function () {
+        const show = $(this).children("div").eq(0)
+        const overlay = $(this).children("div").eq(1)
+        const itemTop = $(".items").offset().top
+        const showTop = $(this).offset().top
+        show.css({
+            top: showTop - itemTop
+        })
+        show.addClass("clicked")
+        overlay.addClass("overlayed")
+    })
+    $(".images-list--overlay").on("click", function (event) {
+        event.stopPropagation()
+        $(".clicked").removeClass("clicked")
+        $(this).removeClass("overlayed")
     })
 })
